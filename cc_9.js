97afceb4b980752e2ce8986427731f81eed4ef8d
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-23 22:49:18 -0500

    "Task 2 - Created Manager Class with Inheritance"

--- cc_9.js
+++ cc_9.js
@@ -19,3 +19,23 @@ class Employee {
 const emp1 = new Employee("Jack Daniels", 101, "Sales", 50000);
 console.log(emp1.getDetails()); 
 console.log(emp1.calculateAnnualSalary()); 
+
+// Task 2: Creating Manager Class (Inheritance & Method Overriding)
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+    
+    getDetails() {
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    }
+    
+    calculateBonus() {
+        return this.calculateAnnualSalary() * 0.1;
+    }
+}
+
+const mgr1 = new Manager("Payton Pritchard", 201, "IT", 90000, 6);
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus());
